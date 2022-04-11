25ad1a6e0c9805f2f693314c0c377cd9cecbe3e3
Aadonjo <68157395+Aadonjo@users.noreply.github.com>
2022-04-11 21:00:54 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<?php include_once("navigationBar1.html"); ?>
+<?php include_once("NavigationBar1.html"); ?>
 <?php include_once("hatha yoga.html"); ?>
 <?php include_once("vin.html"); ?>
 <?php include_once("POWER.html"); ?>
